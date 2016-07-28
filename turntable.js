213678009707022a3e27d3a6a3252ec141e13ad9
jiaqi.zheng <jiaqi.zheng@qunar.com>
2016-07-28 10:05:58 +0800

    turntable修改

--- turntable.js
+++ turntable.js
@@ -99,10 +99,7 @@ Turntable.prototype = {
       cssData[cssPrefix + 'animation-duration'] = me.opt.transitionTime + 's';
       cssData[cssPrefix + 'animation-iteration-count'] = 'infinite';
       cssData[cssPrefix + 'animation-direction'] = 'normal';
-      $anchor.css(cssData).removeClass('qt-rotate');
-      setTimeout(function() {
-        $anchor.addClass('qt-rotate');
-      }, 0)
+      $anchor.css(cssData).addClass('qt-rotate');
       me.stop = false;
     }
 
